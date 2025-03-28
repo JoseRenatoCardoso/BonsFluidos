a6559eb0689865ca0869472c930aa89e74b2779a
Gabriel Jancoski <106913336+Midvirus@users.noreply.github.com>
2025-03-28 18:21:16 -0300

    API Key

--- Scripts/DataBase.js
+++ Scripts/DataBase.js
@@ -8,14 +8,15 @@ import {
     getDocs 
 } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
-const firebaseConfig = {
-    apiKey: "",
-    authDomain: "",
-    projectId: "",
-    storageBucket: "",
-    messagingSenderId: "", 
-    appId: ""
-};
+ const firebaseConfig = {
+    apiKey: "AIzaSyB3mFywI84hXOvQmz0LDi6dgl704sEbuH8",
+    authDomain: "certificadora-de-competencia.firebaseapp.com",
+    projectId: "certificadora-de-competencia",
+    storageBucket: "certificadora-de-competencia.firebasestorage.app",
+    messagingSenderId: "816825012857",
+    appId: "1:816825012857:web:13d8331e28d984896bbaa8",
+    measurementId: "G-EV20D2FPPH"
+  };
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
